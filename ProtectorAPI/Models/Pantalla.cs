405001cf32882c6112f23208bdebd24d5c75e9e5
quiro <quiro@STEFF>
2025-06-02 15:56:43 -0600

    add Pantalla

---
+++ ProtectorAPI/Models/Pantalla.cs
@@ -0,0 +1,12 @@
+﻿namespace ProtectorAPI.Models
+{
+    public class Pantalla
+    {
+        public int IdPantalla { get; set; }
+        public int IdSistema { get; set; }
+        public string Descripcion { get; set; }
+        public string FotoUrl { get; set; }
+        public char Estado { get; set; }
+
+    }
+}

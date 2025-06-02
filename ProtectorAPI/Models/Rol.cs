7bd7506eb16eb1c241ac0ef3890883c8959db927
kyloren831 <aaronmc080301@hotmail.com>
2025-06-02 15:20:10 -0600

    new models

---
+++ ProtectorAPI/Models/Rol.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProtectorAPI.Models
+{
+    public class Rol
+    {
+        [Key]
+        public int IdRol { get; set; }
+        public string Descripcion { get; set; }
+        public char Estado { get; set; }
+    }
+}
---
+++ ProtectorAPI/Models/Sistema.cs
@@ -0,0 +1,10 @@
+﻿namespace ProtectorAPI.Models
+{
+    public class Sistema
+    {
+        public int IdSistema { get; set; }
+        public string Descripcion { get; set; }
+        public string Url { get; set; }
+        public string Estado { get; set; }
+    }
+}

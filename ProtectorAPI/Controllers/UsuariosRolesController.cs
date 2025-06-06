05d1c069c1e56cde223dd4e494367adb06ea4ca8
jimenezfiorella88@gmail.com <jimenezfiorella88@gmail.com>
2025-06-06 10:53:11 -0600

    Prueba

---
+++ ProtectorAPI/Controllers/UsuariosRolesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProtectorAPI.Controllers
+{
+    public class UsuariosRolesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
